4904976d8183be227c1f981d87b3eafb97054e65
hayshowspeed <124213731+hayshowspeed@users.noreply.github.com>
2023-02-08 19:57:33 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,95 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Sonic":return img`
+. 8 . 8 . . . . 
+8 . 8 8 8 . . . 
+. 8 8 1 8 . . . 
+8 8 8 f d f . . 
+8 . 8 d d . . . 
+1 8 8 8 8 8 1 . 
+. . 8 . 8 . . . 
+. 2 2 . 2 2 . . 
+`;
+            case "image2":
+            case "sonicexe":return img`
+fffffffffffffffffffff
+fffffffffffffffffffff
+fffffffffffffffffffff
+fffffffffffffffffffff
+fffffffffffffffffffff
+ffffffff2222222222222
+ffffffff5555555555555
+ffffffff5555555555555
+ffffffff5555555555555
+ffffffff5151515151515
+88888fff2222222222222
+8d88888f2222222222222
+8dd8888f2225555552222
+888811882225555552222
+888111111225555552522
+888111717225555552222
+8888117ff252222222222
+88dd222d2222222225222
+88dddddf.222222222222
+.dff8ddf.222222222222
+ff11fdd1fffffffffffff
+f1121fd12ffffffffffff
+.f12f8fffffffffffffff
+..fff8fffffffffffffff
+..f8f8fffffffffffffff
+.ff8f8fffffffffffffff
+.111ff1ff.fffffffffff
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "sonicexee":
+            case "anim1":return [img`
+....ff..fffffffff....
+....f8ff888888888f...
+....f888888df888f....
+....f88888ddf88f.....
+...f8888888df8f......
+...188811888888f.....
+...1881111888888f....
+...71871118888888f...
+...211211188888ffff..
+..fff171188888f......
+...fd2222dd8888f.....
+....2dddddd88888f....
+.....fddddffffffff...
+.....fffffdf.........
+....ffdd8ffdf........
+...f2fddf11fdf.......
+...f2fdf2111ff.......
+....f2ff2111f........
+.....ff8f22f.........
+......f8fff..........
+......f8f8f..........
+.....ff8f8ff.........
+....f111f111f........
+...ff11ff111f........
+.ff11ff112222f.......
+f222f22221222f.......
+ffffffffffffff.......
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
